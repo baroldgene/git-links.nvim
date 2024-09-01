4dbb52989641e711d96f2841a5c77e5dbaf04fd1
Barry Brinkley <baroldgene@gmail.com>
2024-09-01 11:38:38 -0600

    Handle multiple lines.

--- lua/gitlinks/utils.lua
+++ lua/gitlinks/utils.lua
@@ -40,6 +40,13 @@ local function fetch_file_info()
   return file_path
 end
 
+local function get_line_number()
+  local starts = vim.fn.line("v")
+  local ends = vim.fn.line(".")
+  -- data.linenumber = vim.api.nvim_win_get_cursor()[1]
+  -- linenumber = vim.api.nvim_win_get_cursor()[1]
+  return { starts = starts, ends = ends }
+end
 
 Utils.get_git_info = function()
   local data = {}
@@ -61,7 +68,7 @@ Utils.get_git_info = function()
 
   data.file_path = fetch_file_info()
 
-  data.linenumber = vim.api.nvim_win_get_cursor(vim.api.nvim_get_current_win())[1]
+  data.linenumber = get_line_number()
 
   return data
 end
@@ -72,8 +79,12 @@ Utils.generate_bitbucket_url = function(git_info)
     'https://' .. git_info.url,
     '/src/' .. git_info.hash,
     '/' .. git_info.file_path,
-    "#lines-" .. git_info.linenumber
+    "#lines-" .. git_info.linenumber.starts
   }
+  if (git_info.linenumber.starts ~= git_info.linenumber.ends) then
+    url[#url + 1] = ":"
+    url[#url + 1] = git_info.linenumber.ends
+  end
   return table.concat(url, "")
 end
 
@@ -82,8 +93,12 @@ Utils.generate_github_url = function(git_info)
     'https://' .. git_info.url,
     '/blob/' .. git_info.hash,
     '/' .. git_info.file_path,
-    "#L" .. git_info.linenumber
+    "#L" .. git_info.linenumber.starts
   }
+  if (git_info.linenumber.starts ~= git_info.linenumber.ends) then
+    url[#url + 1] = "-L"
+    url[#url + 1] = git_info.linenumber.ends
+  end
   return table.concat(url, "")
 end
 
