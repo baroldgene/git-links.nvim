9105918d203598ce11daf291e3bc45d222cc046d
Barry Brinkley <baroldgene@gmail.com>
2024-09-08 11:51:42 -0600

    Optimization
    Used Stubs and Mocks
    Still working on testing failure modes

--- tests/config_spec.lua
+++ tests/config_spec.lua
@@ -6,34 +6,56 @@ local find_map = function(maps, input)
   end
 end
 
+local assert = require("luassert")
+local mock = require('luassert.mock')
+local stub = require('luassert.stub')
+local match = require('luassert.match')
+local spy = require('luassert.spy')
 local P = function(var)
   print(vim.inspect(var))
 end
 
+local function tbl_contains(state, arguments)
+  local expected = arguments[1]
+  return function(tbl)
+    P(tbl)
+    P(expected)
+    for index, value in ipairs(tbl) do
+      if value == expected then
+        return true
+      end
+    end
+  end
+end
+
+assert:register("matcher", "tbl_contains", tbl_contains)
 describe("git-links", function()
   it("Disables keymap when desired", function()
+    local set_keymap = spy.on(vim.keymap, "set")
     require("git-links").setup({ hotkey = "" })
-    local maps = vim.api.nvim_get_keymap("n")
-    local found = find_map(maps, "<Cmd>GenerateGitLink<CR>")
-
-    if (found ~= nil) then
-      P(found)
-    end
-
-    assert.is_true(found == nil)
+    assert.spy(set_keymap).was_called(0)
   end)
 
   it("Has a default keymap", function()
+    local set_keymap = spy.on(vim.keymap, "set")
     require("git-links").setup({})
-    vim.g.mapleader = " "
-    local maps = vim.api.nvim_get_keymap("n")
-    local found = find_map(maps, "<Cmd>GenerateGitLink<CR>")
-
-    assert.is_false(found == nil)
+    assert.spy(set_keymap).was_called(1)
+    assert.spy(set_keymap).was_called_with(match._, "<leader>gw", "<cmd>GenerateGitLink<cr>", match._)
   end)
 
   -- TODO: Implement This
   it("Gets a proper github URL", function()
+    local revert_func = vim.sytem
+    vim.system = function(...)
+      local args = { ... }
+      print("Arg List:")
+      P(arg)
+      return revert_func(arg)
+    end
+    link = require("git-links").generate_url()
+    print("Link:")
+    print(link)
+    vim.system = revert_func
 
   end)
 
